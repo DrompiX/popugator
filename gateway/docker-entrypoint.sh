3765d32ca93a5766cbe92acff22ff02286e10757
Dima T <mrlocky98@gmail.com>
2022-10-09 20:58:25 +0300

    Add db service dependency to gateway

--- gateway/docker-entrypoint.sh
+++ gateway/docker-entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -e
 
+echo "Waiting for infrastructure to initialize"
+/app/common/scripts/wait-for.sh db:5432 -t 20
+echo "Infrastructure was successfully initialized, starting service..."
+
 # Run everything passed via docker command
 exec "$@"
