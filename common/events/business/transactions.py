4e07319db1608efbce5cff71c50ef7059e1c6c3b
Dima T <mrlocky98@gmail.com>
2022-10-20 01:06:24 +0300

    Add new event type

---
+++ common/events/business/transactions.py
@@ -0,0 +1,14 @@
+from enum import Enum
+from pydantic import BaseModel
+
+
+class TransactionType(str, Enum):
+    PROFIT = 'profit'
+    WITHDRAWAL = 'withdrawal'
+    PAYMENT = 'payment'
+
+
+class TransactionApplied(BaseModel):
+    public_user_id: str
+    type: TransactionType
+    amount: int
