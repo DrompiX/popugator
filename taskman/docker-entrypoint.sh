01cedfb0003e03b624e9164246f964f1db585b04
Dima T <mrlocky98@gmail.com>
2022-10-20 01:05:37 +0300

    Add broken docker configurations

--- taskman/docker-entrypoint.sh
+++ taskman/docker-entrypoint.sh
@@ -7,4 +7,4 @@ common/scripts/wait-for.sh kafka1:9094 -t 20
 echo "Infrastructure was successfully initialized, starting service..."
 
 # Run API worker
-uvicorn gateway.asgi:app --host 0.0.0.0 --port 8080 --workers 1
+uvicorn taskman.asgi:app --host 0.0.0.0 --port 8080 --workers 1
